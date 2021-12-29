24b586760c8d2fd231ecdeaae3760696337725c2
Paolo Gariboldi <gariboldipaolopaolo@gmail.com>
2021-12-30 00:05:01 +0100

    Refactor ui

--- src/layouts/dashboard/data/reportsLineChartData.js
+++ src/layouts/dashboard/data/reportsLineChartData.js
@@ -14,6 +14,12 @@ Coded by www.creative-tim.com
 */
 
 export default {
-  labels: ["M", "T", "W", "T", "F", "S", "S"],
-  datasets: { label: "Sales", data: [50, 20, 10, 22, 50, 10, 40] },
+  sales: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: { label: "Mobile apps", data: [50, 40, 300, 320, 500, 350, 200, 230, 500] },
+  },
+  tasks: {
+    labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: { label: "Desktop apps", data: [50, 40, 300, 220, 500, 250, 400, 230, 500] },
+  },
 };
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,28 @@
 /* eslint-disable */
 import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+
+// Data
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
-import { useState } from "react";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+
+// Dashboard components
+import Projects from "layouts/dashboard/components/Projects";
+import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+import {useState} from "react";
 import Button from "@mui/material/Button";
 
 function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
   const [result, setResult] = useState(0);
   const date = Date.now();
   const lastRunDate = `Il risultato risale all'operazione lanciata in data ${new Date(date)}`;
@@ -59,32 +73,100 @@ function Dashboard() {
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={4}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                  icon="leaderboard"
+                  title="Today's Users"
+                  count="2,300"
+                  percentage={{
+                    color: "success",
+                    amount: "+3%",
+                    label: "than last month",
+                  }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                  color="success"
+                  icon="store"
+                  title="Revenue"
+                  count="34k"
+                  percentage={{
+                    color: "success",
+                    amount: "+1%",
+                    label: "than yesterday",
+                  }}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                  color="primary"
+                  icon="person_add"
+                  title="Followers"
+                  count="+91"
+                  percentage={{
+                    color: "success",
+                    amount: "",
+                    label: "Just updated",
+                  }}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={12} lg={12}>
+            <Grid item xs={12} md={6} lg={4}>
               <MDBox mb={3}>
                 <ReportsBarChart
-                  color="info"
-                  title="Risultato:"
-                  description={description}
-                  date={lastRunDate}
-                  chart={reportsBarChartData}
+                    color="info"
+                    title="website views"
+                    description="Last Campaign Performance"
+                    date="campaign sent 2 days ago"
+                    chart={reportsBarChartData}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                    color="success"
+                    title="daily sales"
+                    description={
+                      <>
+                        (<strong>+15%</strong>) increase in today sales.
+                      </>
+                    }
+                    date="updated 4 min ago"
+                    chart={sales}
+                />
+              </MDBox>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                    color="dark"
+                    title="completed tasks"
+                    description="Last Campaign Performance"
+                    date="just updated"
+                    chart={tasks}
                 />
               </MDBox>
             </Grid>
           </Grid>
         </MDBox>
-      </MDBox>
-      <MDBox py={3}>
-        <MDBox mt={4.5}>
+        <MDBox mt={0}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={12} lg={12}>
               <MDBox mb={3}>
                 <Button variant="contained" component="label">
-                  Upload File
-                  <input type="file" hidden onChange={(e) => handleFile(e)} />
+                  <input type="file" onChange={(e) => handleFile(e)} />
                 </Button>
-                <h2>nome file</h2>
               </MDBox>
             </Grid>
           </Grid>
